9c4ab2b61cc0d28deec555b1a95ac41280b3d548
Ahmed NaSr <ahmedalaaeldeen66@gmail.com>
2020-07-11 23:45:39 +0200

    TaskUpdated

---
+++ MyBelongingsApp/Attach/MyTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MyBelongingsApp.Attach
+{
+    public class MyTimer : Timer
+    {
+        public MyTimer()
+        {
+            this.Enabled = true;
+            this.Interval = 5000;
+            this.Start();
+            this.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+        }
+    }
+}
